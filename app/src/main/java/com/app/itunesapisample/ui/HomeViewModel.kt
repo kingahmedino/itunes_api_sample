61fd4cf2460f491746407c5d35083c643542f974
kingahmedino <mohammedoluwasesan@gmail.com>
2020-12-18 21:26:24 +0100

    Added view model for home fragment

---
+++ app/src/main/java/com/app/itunesapisample/ui/HomeViewModel.kt
@@ -0,0 +1,27 @@
+package com.app.itunesapisample.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.app.itunesapisample.models.Track
+import com.app.itunesapisample.repository.TracksRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class HomeViewModel: ViewModel() {
+    private val viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+    val tracks = MutableLiveData<List<Track>>()
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    private fun getTracks(query: String) {
+        uiScope.launch {
+            tracks.postValue(TracksRepository.searchItem(query))
+        }
+    }
+}
