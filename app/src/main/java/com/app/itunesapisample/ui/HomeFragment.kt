022d3642df55b9aba28f189c240a562ab04f6e9f
kingahmedino <mohammedoluwasesan@gmail.com>
2020-12-19 19:26:38 +0100

    Removed appbar and menu item and added SearchView

--- app/src/main/java/com/app/itunesapisample/ui/HomeFragment.kt
+++ app/src/main/java/com/app/itunesapisample/ui/HomeFragment.kt
@@ -1,10 +1,8 @@
 package com.app.itunesapisample.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.app.itunesapisample.databinding.FragmentHomeBinding
